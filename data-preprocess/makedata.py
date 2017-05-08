70b9ef513811b42eed4fcfa935cb6f88207aef2d
Lequn Chen <chenlequn22@gmail.com>
2017-05-08 12:00:54 +0800

    revert to handwritten reader

--- data-preprocess/makedata.py
+++ data-preprocess/makedata.py
@@ -8,7 +8,6 @@ import os
 import multiprocessing
 from collections import namedtuple, defaultdict
 import numpy as np
-import tensorflow as tf
 from tqdm import tqdm
 
 def get_ddhhmm(s):
@@ -103,28 +102,14 @@ def make_feature_map():
     num_features = next_index
 
 
-def write_line(f, writer, imp):
-    index_list = []
-    value_list = []
+def write_line(f, imp):
     f.write(imp.label + ' ')
     for feature in imp.features:
         idx = feature_map.get(feature, None)
         if idx is None:
             idx = feature_map[FeaturePair(feature.field, '__other__')]
         f.write(str(idx) + ':1 ')
-        index_list.append(idx)
-        value_list.append(1.0)
     f.write('\n')
-
-    index = tf.train.Feature(int64_list=tf.train.Int64List(value=index_list))
-    value = tf.train.Feature(float_list=tf.train.FloatList(value=value_list))
-    label = tf.train.Feature(int64_list=tf.train.Int64List(value=[int(imp.label)]))
-    example = tf.train.Example(features=tf.train.Features(feature={
-        'index': index,
-        'value': value,
-        'label': label,
-    }))
-    writer.write(example.SerializeToString())
 
 
 def setup_cursor(dbpath):
@@ -167,27 +152,23 @@ def main(args):
     np.random.shuffle(train)
 
     print 'making train and val data...'
-    tfrecord_options = tf.python_io.TFRecordOptions(tf.python_io.TFRecordCompressionType.GZIP)
     with gzip.open(os.path.join(args.output_dir, 'train.txt.gz'), 'w') as f_train, \
-         gzip.open(os.path.join(args.output_dir, 'val.txt.gz'), 'w') as f_val, \
-         tf.python_io.TFRecordWriter(os.path.join(args.output_dir, 'train.tfrecord.gz'), tfrecord_options) as writer_train, \
-         tf.python_io.TFRecordWriter(os.path.join(args.output_dir, 'val.tfrecord.gz'), tfrecord_options) as writer_val:
+         gzip.open(os.path.join(args.output_dir, 'val.txt.gz'), 'w') as f_val:
         for imp in tqdm(train):
             if imp.clickTime[:2] == '30': # Day 30 as validation set
-                write_line(f_val, writer_val, imp)
+                write_line(f_val, imp)
             else:
-                write_line(f_train, writer_train, imp)
+                write_line(f_train, imp)
     
     print 'making test data...'
     with open(os.path.join(args.input_dir, 'test.csv')) as fin, \
-         gzip.open(os.path.join(args.output_dir, 'test.txt.gz'), 'w') as fout, \
-         tf.python_io.TFRecordWriter(os.path.join(args.output_dir, 'test.tfrecord.gz'), tfrecord_options) as writer_test:
+         gzip.open(os.path.join(args.output_dir, 'test.txt.gz'), 'w') as fout:
         reader = csv.DictReader(fin)
         for row in tqdm(reader, total=get_num_lines(os.path.join(args.input_dir, 'test.csv'))):
             features, label, clickTime = make_impression(row)
             label = row['instanceID'] # little hack to use write_line()
             imp = Impression(features, label, clickTime)
-            write_line(fout, writer_test, imp)
+            write_line(fout, imp)
     
     print 'writing metadata...'
     with open(os.path.join(args.output_dir, 'num_ones.txt'), 'w') as f:
--- model/model_20170504_clq_naive_lr.py
+++ model/model_20170504_clq_naive_lr.py
@@ -14,71 +14,106 @@ import tensorflow as tf
 
 MODEL_NAME = os.path.splitext(os.path.basename(__file__))[0]
 
+class BufferedDataReader(object):
+    def __init__(self, filename, batch_size, num_one, batch_buffer_size=100, num_batch_worker=4):
+        self._line_queue = multiprocessing.Queue(batch_buffer_size * batch_size)
+        self._batch_queue = multiprocessing.Queue(batch_buffer_size)
+        self._batch_maker = []
+        if batch_size == -1:
+            batch_size = sys.maxint
+        for _ in xrange(num_batch_worker):
+            p = multiprocessing.Process(target=BufferedDataReader._batch_worker,
+                                        args=(self._line_queue, self._batch_queue, batch_size, num_one))
+            self._batch_maker.append(p)
+            p.start()
+        self._line_reader = multiprocessing.Process(target=BufferedDataReader._line_worker,
+                                                    args=(self._line_queue, filename, len(self._batch_maker)))
+        self._line_reader.start()
+
+    def qsize(self):
+        return self._line_queue.qsize(), self._batch_queue.qsize()
+
+    def stop(self):
+        self._line_reader.terminate()
+        for p in self._batch_maker:
+            p.terminate()
+
+    def join(self):
+        self._line_reader.join()
+        for p in self._batch_maker:
+            p.join()
+
+    def get_batch(self):
+        xs, ys = self._batch_queue.get()
+        return xs, ys
+
+    @staticmethod
+    def _batch_worker(line_queue, batch_queue, batch_size, num_one):
+        run = True
+        while run:
+            xs = np.zeros((batch_size, num_one), dtype=int)
+            ys = np.empty(batch_size, dtype=int)
+            for i in xrange(batch_size):
+                line = line_queue.get()
+                if line is None:
+                    xs, ys = xs[:i], ys[:i]
+                    run = False
+                    break
+
+                split = line.split()
+                clicked = int(split[0])
+                for j, s in enumerate(split[1:]):
+                    k, v = s.split(':')
+                    xs[i, j] = int(k)
+                ys[i] = clicked
+            batch_queue.put((xs, ys))
+            run = run and len(ys) != 0
+        batch_queue.put(([], []))
+        batch_queue.close()
+
+    @staticmethod
+    def _line_worker(line_queue, filename, num_batch_maker):
+        with gzip.open(filename) as f:
+            for line in f:
+                line_queue.put(line)
+        for _ in xrange(num_batch_maker):
+            line_queue.put(None)
+        line_queue.close()
+
 
 class LinearRegressionCTR(object):
     def __init__(self, **kwargs):
         self._data_root = kwargs['data_root']
         self._output_root = kwargs['output_root']
         self._num_feature = kwargs['num_feature']
+        self._num_one = kwargs['num_one']
         self._learning_rate = kwargs['learning_rate']
         self._batch_size = kwargs['batch_size']
 
         self._build()
 
-    def _inputs(self, filenames):
-        filename_queue = tf.train.string_input_producer(filenames)
-        options = tf.python_io.TFRecordOptions(tf.python_io.TFRecordCompressionType.GZIP)
-        reader = tf.TFRecordReader(options=options)
-        _, serialized_example = reader.read(filename_queue)
-        batch_serialized_examples = tf.train.batch([serialized_example],
-                                                   batch_size=self._batch_size,
-                                                   num_threads=8,
-                                                   capacity=self._batch_size * 128)
-        proto = {
-            'index': tf.VarLenFeature(tf.int64),
-            'value': tf.VarLenFeature(tf.float32),
-            'label': tf.FixedLenFeature(shape=[1], dtype=tf.int64),
-        }
-        features = tf.parse_example(batch_serialized_examples, proto)
-        indicies = features['index']
-        values = features['value']
-        labels = tf.cast(tf.squeeze(features['label'], axis=1), tf.float32)
-        return indicies, values, labels
-
     def _build(self):
-        filename = os.path.join(self._data_root, 'train.tfrecord.gz')
-        indicies, values, labels = self._inputs([filename])
-
-        # self._ph_x = tf.placeholder(tf.int32, [None, None])
-        # self._ph_y = tf.placeholder(tf.float32, [None])
+        self._ph_x = tf.placeholder(tf.int32, [None, self._num_one])
+        self._ph_y = tf.placeholder(tf.float32, [None])
         w = tf.get_variable('weight', [self._num_feature], dtype=tf.float32,
                             initializer=tf.random_uniform_initializer(-0.05, 0.05))
         b = tf.get_variable('bias', [1], dtype=tf.float32,
                             initializer=tf.zeros_initializer())
-        wx = tf.nn.embedding_lookup_sparse(params=w, sp_ids=indicies, sp_weights=values, combiner='sum')
-        # wx = tf.sparse_tensor_dense_matmul(self._ph_x, w)
-        # wx = tf.reduce_sum(tf.gather(w, self._ph_x), axis=1)
+        wx = tf.reduce_sum(tf.gather(w, self._ph_x), axis=1)
         logits = wx + b
         prob = tf.sigmoid(logits)
-        loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=labels, logits=logits))
-        # loss = tf.reduce_mean(tf.nn.weighted_cross_entropy_with_logits(targets=labels, logits=logits, pos_weight=100))
+        loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=self._ph_y, logits=logits))
         opt = tf.train.AdamOptimizer(self._learning_rate)
         self._train_step = opt.minimize(loss)
-        self._auc, self._auc_op = tf.metrics.auc(labels, prob)
+        self._auc, self._auc_op = tf.metrics.auc(self._ph_y, prob)
         tf.summary.scalar('train_loss', loss)
         tf.summary.scalar('train_auc', self._auc)
         self._summary_step = tf.summary.merge_all()
 
         self._model_dir = os.path.join(self._output_root, 'models', datetime.now().strftime('%Y-%m-%d_%H-%M-%S') + '_' + MODEL_NAME)
         log_dir = os.path.join(self._output_root, 'logs', datetime.now().strftime('%Y-%m-%d_%H-%M-%S' + '_' + MODEL_NAME))
-        if self._output_root.startswith('gs://'):
-            # create directory for model saver path
-            bucket, path = get_bucket_and_path(os.path.split(self._model_dir)[0])
-            blob = bucket.blob(path)
-            blob.upload_from_string('')
-        else:
-            os.makedirs(self._model_dir)
-            os.makedirs(log_dir)
+        os.makedirs(self._model_dir)
+        os.makedirs(log_dir)
         self._saver = tf.train.Saver()
         config = tf.ConfigProto(allow_soft_placement=True, log_device_placement=False)
         config.gpu_options.allow_growth=True
@@ -92,43 +127,50 @@ class LinearRegressionCTR(object):
         self._saver.save(self._sess, self._model_dir, global_step=self._step)
 
     def train(self):
-        filename = os.path.join(self._data_root, 'train.tfrecord.gz')
-        # xs, ys = self._inputs([filename])
+        filename = os.path.join(self._data_root, 'train.txt.gz')
+        reader = BufferedDataReader(filename, self._batch_size, self._num_one)
         self._sess.run(tf.local_variables_initializer())
-        coord = tf.train.Coordinator()
-        threads = tf.train.start_queue_runners(sess=self._sess, coord=coord)
         try:
-            while not coord.should_stop():
+            while True:
+                batch = reader.get_batch()
+                xs, ys = batch
+                if len(ys) == 0:
+                    break
                 self._step += 1
-                _, _, summary = self._sess.run([self._train_step, self._auc_op, self._summary_step])
-                # _, _, summary = self._sess.run([self._train_step, self._auc_op, self._summary_step],
-                #                                feed_dict={self._ph_x: xs, self._ph_y: ys})
+                _, _, summary = self._sess.run([self._train_step, self._auc_op, self._summary_step],
+                                               feed_dict={self._ph_x: xs, self._ph_y: ys})
                 if self._step % 16 == 0:
                     self._sess.run(tf.local_variables_initializer())
                     self._summary_writer.add_summary(summary, self._step)
-        except tf.errors.OutOfRangeError:
-            pass
-        finally:
-            coord.request_stop()
-        coord.join(threads)
+                    _, num_batch_buffer = reader.qsize()
+                    summary = tf.Summary(value=[tf.Summary.Value(tag='batch_buffer', simple_value=num_batch_buffer)])
+                    self._summary_writer.add_summary(summary, self._step)
+        except:
+            reader.stop()
+            reader.join()
+            raise
+        reader.stop()
+        reader.join()
 
     def validate(self):
-        filename = os.path.join(self._data_root, 'val.tfrecord.gz')
-        xs, ys = self._inputs(filename, self._batch_size)
+        filename = os.path.join(self._data_root, 'val.txt.gz')
+        reader = BufferedDataReader(filename, self._batch_size, self._num_one)
         self._sess.run(tf.local_variables_initializer())
-        coord = tf.train.Coordinator()
-        threads = tf.train.start_queue_runners(self._sess, coord=coord)
         try:
-            while not coord.should_stop():
+            while True:
+                xs, ys = reader.get_batch()
+                if len(ys) == 0:
+                    break
                 self._sess.run(self._auc_op, feed_dict={self._ph_x: xs, self._ph_y: ys})
             auc = self._sess.run(self._auc)
             summary = tf.Summary(value=[tf.Summary.Value(tag='val_auc', simple_value=auc)])
             self._summary_writer.add_summary(summary, self._step)
-        except tf.errors.OutOfRangeError:
-            pass
-        finally:
-            coord.request_stop()
-        coord.join(threads)
+        except:
+            reader.stop()
+            reader.join()
+            raise
+        reader.stop()
+        reader.join()
 
 
 def main():
@@ -136,6 +178,7 @@ def main():
     parser.add_argument('--data_root', type=str, required=True)
     parser.add_argument('--output_root', type=str, required=True)
     parser.add_argument('--num_feature', type=int, required=True)
+    parser.add_argument('--num_one', type=int, required=True)
     args, _ = parser.parse_known_args()
 
     data_root = args.data_root
@@ -143,6 +186,7 @@ def main():
     model = LinearRegressionCTR(data_root=data_root,
                                 output_root=output_root,
                                 num_feature=args.num_feature,
+                                num_one=args.num_one,
                                 learning_rate=5e-4,
                                 batch_size=512)
     for _ in xrange(10):
