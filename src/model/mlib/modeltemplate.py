af76ef3cfec362e18a701a701c529dce54beb7c3
Tianyao Chen <try.skycn@Gmail.com>
2017-06-01 12:30:17 +0800

    codes for test

--- src/model/mlib/modeltemplate.py
+++ src/model/mlib/modeltemplate.py
@@ -9,129 +9,118 @@ class KeyMap:
 		assert k in cls.MAP, "{} {} not found.".format(cls.NAME, k)
 		return cls.MAP[k]
 
-class Initializers(KeyMap):
-	NAME = "Initializer"
+class Optimizers(KeyMap):
+	NAME = "Optimizer"
 	MAP = {
 		"adam": tf.train.AdamOptimizer
 	}
 
 class ModelTemplate(object):
-	def __init__(self, **kwargs):
-		self._data_root = kwargs['data_root']
-		self._output_root = kwargs['output_root']
-		self._num_one = kwargs['num_one']
-		self._optimizer = kwargs['optimizer']
-		self._learning_rate = kwargs['learning_rate']
-		self._batch_size = kwargs['batch_size']
+	def __init__(self, data_storage, config):
+		self._rep = data_storage.get_representation(IndexRepresentation)
+		self._num_feature = self._rep.dense_shape
+		self._num_one = self._rep.max_length
+		self._learning_rate = config['learning_rate']
+		self._batch_size = config['batch_size']
+		self._optimizer = config.get('initializer', 'adam')
+		self._session_name = datetime.now().strftime('%Y-%m-%d_%H-%M-%S') + '_' + config['module_name']
+		self._model_dir = os.path.join(config['models_dir'], self._session_name)
+		self._log_dir = os.path.join(config['logs_dir'], self._session_name)
+		self._train_log_dir = os.path.join(self._log_dir, "train")
+		self._val_log_dir = os.path.join(self._log_dir, "val")
+		self._test_dir = config['tests_dir']
+
+		self._makefolders()
 
 		self._build()
 
-    def _body(self):
-    	pass
+	def _makefolders(self):
+		os.makedirs(self._model_dir)
+		os.makedirs(self._log_dir)
+		os.makedirs(self._train_log_dir)
+		os.makedirs(self._val_log_dir)
+		if not os.path.exists(self._test_dir):
+			os.makedirs(self._test_dir)
+
+	def _body(self):
+		w = tf.get_variable('weight', [self._num_feature], dtype=tf.float32,
+							initializer=tf.random_uniform_initializer(-0.05, 0.05))
+		b = tf.get_variable('bias', [1], dtype=tf.float32,
+							initializer=tf.zeros_initializer())
+		wx = tf.reduce_sum(tf.gather(w, self._ph_x), axis=1)
+		return wx + b
 
 	def _build(self):
-		self._ph_x = tf.placeholder(tf.int32, [None, self._num_one], name="feed_x")
-		self._ph_y = tf.placeholder(tf.float32, [None], name="feed_y")
-
-		logits = self._body();
-		self._global_step = tfutils.make_global_step()
-
+		self._ph_x = tf.placeholder(tf.int32, [None, self._num_one])
+		self._ph_y = tf.placeholder(tf.float32, [None])
+		logits = self._body()
 		self._prob = tf.sigmoid(logits)
-		self._loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=self._ph_y, logits=logits))
-		opt = Initializers.get(self._optimizer)(self._learning_rate, global_step=self._global_step)
-		self._train_step = opt.minimize(self._loss)
-		self._auc, self._auc_op = tf.metrics.auc(self._ph_y, self._prob)
+		loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=self._ph_y, logits=logits))
+		self._train_step = Optimizers.get(self._optimizer)(self._learning_rate).minimize(loss)
+		avgloss, _ = tf.metrics.mean(loss, updates_collections=tf.GraphKeys.UPDATE_OPS)
+		auc, _ = tf.metrics.auc(self._ph_y, self._prob, updates_collections=tf.GraphKeys.UPDATE_OPS)
+		tf.summary.scalar("avg_loss", avgloss)
+		tf.summary.scalar("auc", auc)
+		self._summary_op = tf.summary.merge_all()
+		self._update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
 
-		self._session_name = datetime.now().strftime('%Y-%m-%d_%H-%M-%S') + '_' + self.__class__.MODEL_NAME
-		self._model_dir = os.path.join(self._output_root, 'models', self._session_name)
-		os.makedirs(self._model_dir)
-		log_dir = os.path.join(self._output_root, 'logs', self._session_name)
-		train_log_dir = os.path.join(log_dir, "train")
-		os.makedirs(train_log_dir)
-		self._train_summary_writer = tf.summary.FileWriter(train_log_dir, self._sess.graph, flush_secs=2)
-		val_log_dir = os.path.join(log_dir, "val")
-		os.makedirs(val_log_dir)
-		self._val_summary_writer = tf.summary.FileWriter(val_log_dir, flush_secs=2)
-
-		self._saver = tf.train.Saver()
-		self._sess = tfutils.create_session()
-
+		self._global_step = tfutils.make_global_step()
 		self._global_initializer = tf.global_variables_initializer()
 		self._local_initializer = tf.local_variables_initializer()
 
+		self._saver = tf.train.Saver()
+		self._sess = tfutils.create_session()
+		self._train_sum_writer = tf.summary.FileWriter(self._train_log_dir, self._sess.graph, flush_secs=2)
+		self._val_sum_writer = tf.summary.FileWriter(self._val_log_dir, flush_secs=2)
+
 		self._sess.run(self._global_initializer)
 		self._step = 0
-	
+
+	def _get_dataset(self, dataset):
+		return self._rep.get_dataset(dataset, self._batch_size)
+
+	def _recover_sess(self):
+		sess = tfutils.create_session()
+		sess.run(self._global_initializer)
+		tfutils.save_session(sess, self._saver, self._model_dir, self._step)
+		tfutils.load_session(sess, self._saver, self._model_dir)
+		return sess
+
 	def load(self, model_path):
 		tfutils.load_session(self._sess, self._saver, model_path)
 
 	def save(self):
-		tfutils.save_session(self._sess, self._saver, model_path, self._step)
+		tfutils.save_session(self._sess, self._saver, self._model_dir, self._step)
 
-	def train(self):
-		filename = os.path.join(self._data_root, 'train.txt.gz')
-		reader = BufferedDataReader(filename, self._batch_size, self._num_one)
-		self._sess.run(self._local_initializer)
-		try:
-			while True:
-				xs, ys = reader.get_batch()
-				if len(ys) == 0:
-					break
-				_, _, loss, auc, self._step = self._sess.run([self._train_step, self._auc_op,
-															  self._loss, self._auc, self._global_step],
-															 feed_dict={self._ph_x: xs, self._ph_y: ys})
-				if self._step % 16 == 0:
-					self._sess.run(tf.local_variables_initializer())
-					_, num_batch_buffer = reader.qsize()
-					summary = tf.Summary(value=[tf.Summary.Value(tag="loss", simple_value=loss),
-												tf.Summary.Value(tag="auc", simple_value=auc),
-												tf.Summary.Value(tag="batch_buffer", simple_value=num_batch_buffer)])
-					self._summary_writer.add_summary(summary, self._step)
-		finally:
-			reader.stop()
-			reader.join()
+	def train(self, dataset):
+		self._sess.run(tf.local_variables_initializer())
+		for xs, ys in self._get_dataset(dataset):
+			_, _, self._step = self._sess.run([self._train_step, self._update_ops, self._global_step],
+												 feed_dict={self._ph_x: xs, self._ph_y: ys})
+			if self._step % 16 == 0:
+				self._sess.run(self._local_initializer)
+				summary = self._sess.run(self._summary_op)
+				self._train_sum_writer.add_summary(summary, self._step)
+				self._train_sum_writer.flush()
 
-	def validate(self):
-		valsess = tfutils.create_session()
-		valsess.run(self._global_initializer)
-		tfutils.load_session(valsess, self._saver, self._model_dir)
-		filename = os.path.join(self._data_root, 'val.txt.gz')
-		reader = BufferedDataReader(filename, self._batch_size, self._num_one)
-		self._sess.run(self._local_initializer)
-		try:
-			while True:
-				xs, ys = reader.get_batch()
-				if len(ys) == 0:
-					break
-				self._sess.run(self._auc_op, feed_dict={self._ph_x: xs, self._ph_y: ys})
-			auc = self._sess.run(self._auc)
-			summary = tf.Summary(value=[tf.Summary.Value(tag='val_auc', simple_value=auc)])
-			self._summary_writer.add_summary(summary, self._step)
-		finally:
-			reader.stop()
-			reader.join()
-	
-	def test(self):
+	def validate(self, dataset):
+		sess = self._recover_sess()
+		sess.run(self._local_initializer)
+		for xs, ys in self._get_dataset(dataset):
+			sess.run(self._update_ops, feed_dict={self._ph_x: xs, self._ph_y: ys})
+		summary = sess.run(self._summary_op)
+		self._val_sum_writer.add_summary(summary, self._step)
+		sess.close()
+
+	def test(self, dataset):
+		sess = self._recover_sess()
 		res = []
-		filename = os.path.join(self._data_root, 'test.txt.gz')
-		reader = BufferedDataReader(filename, self._batch_size, self._num_one)
-		try:
-			while True:
-				xs, ys = reader.get_batch()
-				if len(ys) == 0:
-					break
-				probs = self._sess.run(self._prob, feed_dict={self._ph_x: xs})
-				for i, prob in zip(ys, probs):
-					res.append((i, prob))
-		finally:
-			reader.stop()
-			reader.join()
+		for xs, ys in self._get_dataset(dataset):
+			probs = sess.run(self._prob, feed_dict={self._ph_x: xs})
+			res.extend([(i, prob) for i, prob in zip(ys, probs)])
 		res.sort(key=lambda (i, prob): i)
 
-		test_dir = os.path.join(self._output_root, 'tests')
-		if not os.path.exists(test_dir):
-			os.makedirs(test_dir)
-		filename = os.path.join(test_dir, self._session_name)
+		filename = os.path.join(self._test_dir, self._session_name)
 		with open(filename + '.csv', 'w') as f:
 			f.write('instanceID,prob\n')
 			for i, prob in res:
--- src/model/model_20170529_cty_lr/__main__.py
+++ src/model/model_20170529_cty_lr/__main__.py
@@ -1,45 +1,32 @@
-import mlib
+from .. import mlib
 
 class LinearRegressionCTR(mlib.ModelTemplate):
-	def __init__(self, **kwargs):
-		self._num_feature = kwargs.pop("num_feature")
-		super(self.__class__, self).__init__(self, kwargs)
+    pass
 
-	def _body(self):
-		w = tf.get_variable('weight', [self._num_feature], dtype=tf.float32,
-							initializer=tf.random_uniform_initializer(-0.05, 0.05))
-		b = tf.get_variable('bias', [1], dtype=tf.float32,
-							initializer=tf.zeros_initializer())
-		wx = tf.reduce_sum(tf.gather(w, self._ph_x), axis=1)
-		return wx + b
-
-def main():
+def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data_root', type=str, required=True)
-    parser.add_argument('--output_root', type=str, required=True)
-    parser.add_argument('--num_feature', type=int, required=True)
-    parser.add_argument('--num_one', type=int, required=True)
     parser.add_argument('--train', action='store_true')
     parser.add_argument('--model', type=str, help='load model from the given path')
     args, _ = parser.parse_known_args()
+    return args
 
-    data_root = args.data_root
-    output_root = args.output_root
-    model = LinearRegressionCTR(data_root=data_root,
-                                output_root=output_root,
-                                num_feature=args.num_feature,
-                                num_one=args.num_one,
-                                learning_rate=5e-4,
-                                batch_size=512)
+def main():
+    config = read_module_config(__file__, 'model.json')
+    data_storage = DataStorage(config['features'])
+    args = parse_args()
+
+    model = LogisticRegressionCTR(data_storage)
+    data_storage.load_data()
+    print('feature data loaded')
     if args.model:
         model.load(args.model)
     if args.train:
         print('training...')
-        for _ in range(10):
-            model.train()
+        for _ in range(config['epoch']):
+            model.train('smalltrain1')
             model.save()
-            model.validate()
-    model.test()
+            model.validate('val1')
+    print 'testing...'
+    model.test('test')
 
-if __name__ == '__main__':
-    main()
+main()
